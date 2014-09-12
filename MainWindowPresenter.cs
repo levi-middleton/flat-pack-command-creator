bad2cfba4217236fb7e895b553697eba19addc3a
Levi Middleton <levi@theawesomefiles.com>
2014-09-12 15:19:27 -0400

    First version.

---
+++ MainWindowPresenter.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatPackCommandCreator
+{
+	public class MainWindowPresenter : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, args);
+
+			HandleDependentProperties(args);
+		}
+
+		private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+		}
+
+		private bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (Equals(storage, value))
+				return false;
+
+			storage = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+
+		private void HandleDependentProperties(PropertyChangedEventArgs args)
+		{
+			switch (args.PropertyName)
+			{
+				case "InputText":
+					{
+						SetOutputText(InputText);
+					}
+					break;
+				case "OutputText":
+					{
+						IsInvalid = OutputText.Length > 32767;
+					}
+					break;
+			}
+		}
+
+		private string _inputText = string.Empty;
+		public string InputText
+		{
+			get { return _inputText; }
+			set
+			{
+				SetProperty(ref _inputText, value);
+			}
+		}
+
+		private string _outputText = string.Empty;
+		public string OutputText
+		{
+			get { return _outputText; }
+			set
+			{
+				SetProperty(ref _outputText, value);
+			}
+		}
+
+		private bool _isInvalid = false;
+		public bool IsInvalid
+		{
+			get { return _isInvalid; }
+			set
+			{
+				SetProperty(ref _isInvalid, value);
+			}
+		}
+
+		private void SetOutputText(string inboundText)
+		{
+			string[] lines = inboundText.Split('\n');
+			int numberOfBlocks = ((lines.Length - 1) / 2) * 3 + 5;
+
+			//start with the first line
+			string output = ID_PREFIX;
+			output += string.Format(BLOCK, COMMAND_BLOCK);
+			output += string.Format(COMMAND_SUFFIX, lines[0].Replace("\r",string.Empty));
+			output = ID_PREFIX + string.Format(BLOCK, REDSTONE_BLOCK) + string.Format(RIDING_SUFFIX, output);
+
+			//put the rest of the lines in, interspersing redstone blocks
+			for (int i = 1; i < lines.Length; ++i)
+			{
+				output = ID_PREFIX + string.Format(BLOCK, COMMAND_BLOCK) + string.Format(COMMAND_SUFFIX, lines[i].Replace("\r", string.Empty)) + string.Format(RIDING_SUFFIX, output);
+				if (i % 2 == 0)
+				{
+					output = ID_PREFIX + string.Format(BLOCK, REDSTONE_BLOCK) + string.Format(RIDING_SUFFIX, output);
+				}
+			}
+
+			//add an iron block if the top block is redstone
+			if (lines.Length % 2 == 1)
+			{
+				output = ID_PREFIX + string.Format(BLOCK, IRON_BLOCK) + string.Format(RIDING_SUFFIX, output);
+			}
+
+			//add cleanup code
+			output = ID_PREFIX + string.Format(BLOCK, COMMAND_BLOCK) + string.Format(COMMAND_SUFFIX, string.Format(CLEANUP_COMMAND,numberOfBlocks-1)) + string.Format(RIDING_SUFFIX, output);
+			
+			//capstone redstone block
+			output = string.Format(STARTING_BLOCK,string.Format(BLOCK, REDSTONE_BLOCK) + string.Format(RIDING_SUFFIX, output));
+
+			OutputText = output;
+		}
+
+		private static readonly string STARTING_BLOCK = "summon FallingSand ~ ~3 ~ {{{0}}}";
+		private static readonly string BLOCK = "Block:{0},Time:1,DropItem:0";
+		private static readonly string ID_PREFIX = "id:FallingSand,";
+		private static readonly string COMMAND_SUFFIX = ",TileEntityData:{{Command:\"{0}\"}}";
+		private static readonly string RIDING_SUFFIX = ",Riding:{{{0}}}";
+
+		private static readonly string REDSTONE_BLOCK = "redstone_block";
+		private static readonly string IRON_BLOCK = "iron_block";
+		private static readonly string COMMAND_BLOCK = "command_block";
+
+		private static readonly string CLEANUP_COMMAND = "fill ~ ~1 ~ ~ ~-{0} ~ air";
+	}
+}
